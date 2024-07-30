f813a6b4d9c87bfd031ecb0f1e25df8bd7a78576
kimeorua <kimeorua1222@naver.com>
2024-07-30 13:25:46 +0900

    초기 커밋 메시지입니다.

---
+++ Source/SoulslikeGame/SoulslikeGame.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class SoulslikeGame : ModuleRules
+{
+	public SoulslikeGame(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
