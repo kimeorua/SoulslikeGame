04f866b193803fa2f0de64f16bc3994cfe87bc10
kimeorua <kimeorua1222@naver.com>
2024-09-10 12:25:59 +0900

    초기 HP 계산 및 적용 구현

---
+++ Source/SoulslikeGame/GAS/InitHPMagnitudeCalculation.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayModMagnitudeCalculation.h"
+#include "InitHPMagnitudeCalculation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOULSLIKEGAME_API UInitHPMagnitudeCalculation : public UGameplayModMagnitudeCalculation
+{
+	GENERATED_BODY()
+	
+};
