30d341768a6be2f7cfdf408a95b55f52b2723859
kimeorua <kimeorua1222@naver.com>
2024-09-26 19:37:28 +0900

    c++ 추가 및 변경

---
+++ Source/SoulslikeGame/AnimInstance/SoulslikeAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AnimInstance.h"
+#include "SoulslikeAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOULSLIKEGAME_API USoulslikeAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
