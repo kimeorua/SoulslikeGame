a6ee0c8d247a4551fab22d41468e517be7ebed1d
kimeorua <kimeorua1222@naver.com>
2024-09-26 19:37:44 +0900

    애님인스턴스 c++ 추가

--- Source/SoulslikeGame/AnimInstance/SoulslikeAnimInstance.cpp
+++ Source/SoulslikeGame/AnimInstance/SoulslikeAnimInstance.cpp
@@ -1,5 +1,45 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "SoulslikeAnimInstance.h"
+#include "SoulslikeGame/Character/BaseCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "KismetAnimationLibrary.h"
 
-#include "AnimInstance/SoulslikeAnimInstance.h"
+USoulslikeAnimInstance::USoulslikeAnimInstance()
+{
+	MovementComponent = nullptr;
+	GASComponent = nullptr;
+	Character = nullptr;
+}
 
+void USoulslikeAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	
+
+	if (IsValid(Character))
+	{
+		SettingVlaue();
+	}
+	else
+	{
+		Character = Cast<ABaseCharacter>(TryGetPawnOwner());
+		if (IsValid(Character))
+		{
+			MovementComponent = Character->GetCharacterMovement();
+			GASComponent = Character->GetAbilitySystemComponent();
+		}
+		SettingVlaue();
+	}
+}
+
+void USoulslikeAnimInstance::SettingVlaue()
+{
+	if (IsValid(MovementComponent))
+	{
+		Speed = UKismetMathLibrary::VSizeXY(MovementComponent->Velocity);
+		FRotator Rot = FRotator(0.0f, Character->GetActorRotation().Yaw, 0);
+		Direction = UKismetAnimationLibrary::CalculateDirection(MovementComponent->Velocity, Rot);
+		IsAccelerating = UKismetMathLibrary::VSize(MovementComponent->GetCurrentAcceleration()) > 0 ? true : false;
+	}
+}
--- Source/SoulslikeGame/AnimInstance/SoulslikeAnimInstance.h
+++ Source/SoulslikeGame/AnimInstance/SoulslikeAnimInstance.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "AnimInstance.h"
+#include "Animation/AnimInstance.h"
 #include "SoulslikeAnimInstance.generated.h"
 
 /**
@@ -14,4 +14,31 @@ class SOULSLIKEGAME_API USoulslikeAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
 	
+private:
+	// 캐릭터 무브먼트 컴포넌트
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components",  meta = (AllowPrivateAccess = "true"))
+	class UCharacterMovementComponent* MovementComponent;
+
+	// 캐릭터 GAS 컴포넌트
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	class USoluslikeAbilitySystemComponent* GASComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character", meta = (AllowPrivateAccess = "true"))
+	class ABaseCharacter* Character;;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Value", meta = (AllowPrivateAccess = "true"))
+	float Speed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Value", meta = (AllowPrivateAccess = "true"))
+	float Direction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Value", meta = (AllowPrivateAccess = "true"))
+	bool IsAccelerating;
+
+public:
+	USoulslikeAnimInstance(); // 생성자
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override; //애니메이션 업데이트 함수
+
+private:
+	void SettingVlaue();
 };
